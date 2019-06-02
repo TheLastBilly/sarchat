8192a2a657e6f8572710ab4e7d35260fd0886fb9
TheLastBilly <jtmonegro@gmail.com>
2019-06-02 01:24:41 -0400

    Added license file(s)

--- src/display.cpp
+++ src/display.cpp
@@ -1,3 +1,11 @@
+// sarchat - display.cpp
+//
+// Copyright (c) 2019, TheLastBilly
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #include "include/display.hpp"
 
 namespace Display{
--- src/include/display.hpp
+++ src/include/display.hpp
@@ -1,3 +1,11 @@
+// sarchat - display.hpp
+//
+// Copyright (c) 2019, TheLastBilly
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
